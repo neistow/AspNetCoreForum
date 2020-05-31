d938a93e6ee860baebb0ab890453ae9ebb4a0d3c
unknown <glebchernigin236@gmail.com>
2020-05-31 15:52:33 +0300

    Revert "QuickFix"
    This reverts commit 073cdd7a7fd448fa14a8b1a174e438d1d812ca9e.

--- Forum.Core/Abstract/Managers/IUserManager.cs
+++ Forum.Core/Abstract/Managers/IUserManager.cs
@@ -4,13 +4,15 @@ using Forum.Core.Concrete.Models;
 
 namespace Forum.Core.Abstract.Managers
 {
-    public interface IUserManager : IManager
+    public interface IUserManager
     {
         Task<User> Authenticate(string username, string password);
         Task<List<User>> GetAll();
         ValueTask<User> GetById(int id);
         User Get(int id);
-        bool CreateUser(User user, string password);
+        bool Create(User user, string password);
         void Delete(User user);
+        int SaveChanges();
+        Task<int> SaveChangesAsync();
     }
 }
